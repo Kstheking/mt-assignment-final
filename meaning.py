e7012fbb2391fb6024a8fae9b40c5fcafad7bf4b
kishan75 <kishan.chaurasiya.75@gmail.com>
2021-11-11 02:48:26 +0530

    added url and meaning fetch code in meaning file

--- meaning.py
+++ meaning.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 
@@ -7,11 +8,41 @@ import requests
 import multiprocessing as mp
 from tqdm import tqdm
 from bs4 import BeautifulSoup
-from common import base_url, letter_list, get_subfiles
+from common import link_folder_path, get_subfolders, base_url, get_subfiles
 
 
 meanings_folder_path = "meanings"
-letter_links_folder_path = (meanings_folder_path+'/{letter}')
+
+
+def write_to_json(object, file_path):
+    with open(file_path+".json", "w+") as outfile:
+        json.dump(object, outfile, indent=4, ensure_ascii=False)
+
+
+def get_words_url(file):
+    with open(file['path']) as file:
+        return file.readlines()
+
+
+def fetch_meaning_from_url(url, session):
+    page = session.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    word_english = soup.find('td', {'class': 't_english t_data'}).text
+    word_types = [w_type.text for w_type in soup.find_all(
+        'td', {'class': 't_type t_data'})]
+    word_japaneses = [w_jap.text for w_jap in soup.find_all(
+        'td', {'class': 't_japanese t_data'})]
+    word_hiraganas = [w_hir.text for w_hir in soup.find_all(
+        'td', {'class': 't_hiragana t_data'})]
+    word_pronunciations = [w_pro.text for w_pro in soup.find_all(
+        'td', {'class': 't_pronunciation t_data'})]
+    word_examples = [(w_ex.find('dt').text, w_ex.find('dd').text)
+                     for w_ex in soup.find_all('td', {'class': 't_sentence t_data'})]
+    word_array = [{'type': wt, 'japanese': wh, 'hirangana': wh, 'pronunciation': wp, 'example_eng': we[0], 'example_jap':we[1]}
+                  for wt, wj, wh, wp, we in zip(word_types, word_japaneses, word_hiraganas, word_pronunciations, word_examples)]
+    word_object = {word_english: word_array}
+    return word_object
 
 
 def worker(words_folder):
@@ -23,14 +54,13 @@ def worker(words_folder):
     parent_folder = meanings_folder_path+"/" + words_folder_name
     os.mkdir(parent_folder)
     for file in tqdm(files):
-        fetch_meanings(file, session)
-
-
-def fetch_meanings(file, session):
-    with open(file['name']) as file:
-        lines = file.readlines()
-        lines = [line.strip() for line in lines]
-        
+        words_url = get_words_url(file)
+        meanings = {}
+        for url in words_url:
+            meaning = fetch_meaning_from_url(base_url+url, session)
+            meanings = {**meanings, **meaning}
+        print(meanings)
+        write_to_json(meanings, parent_folder+"/"+file['name'])
 
 
 def meaning_fetch():
@@ -41,7 +71,11 @@ def meaning_fetch():
         shutil.rmtree(path)
         os.makedirs(path)
     p = mp.Pool(26)
-    words_folders = letter_links_folder_path
+    words_folders = get_subfolders(link_folder_path)
+    print(words_folders)
     p.map(worker, words_folders)
     p.close()
     p.join()
+
+
+meaning_fetch()
