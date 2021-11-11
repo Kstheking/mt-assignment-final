228bd258432db45d6bfdc84d6932b94e60460cae
kishan75 <kishan.chaurasiya.75@gmail.com>
2021-11-11 05:37:37 +0530

    comments has added in main function

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ csv_file_path = "english_to_japanease.csv"
 excel_file_path = "english_to_japanease.xlsx"
 
 if __name__ == '__main__':
-    # fetch_words_link()
-    # fetch_meanings()
+    print("----------- words link fetching has started ---------------")
+    fetch_words_link()
+    print("----------- words link fetching has completed and words meaning fetching started  ---------------")
+    fetch_meanings()
+    print("----------- words meaning fetching has completed, words meaning merging has started  ---------------")
     data = get_merged_meaning()
+    print("----------- words meaning merged successfully  ---------------")
     json_to_csv_generator(json_file_path=meanings_folder_path +
                           "/merged.json", csv_file_path=csv_file_path)
     csv_to_excel(csv_file_path, excel_file_path)
