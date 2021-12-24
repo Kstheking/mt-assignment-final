17214f0ab1d74beb60087ee440536699fddf5761
kishan75 <kishan.chaurasiya.75@gmail.com>
2021-12-24 16:21:34 +0530

    added english and hindi words numbering with pronunciation

--- common.py
+++ common.py
@@ -29,49 +29,122 @@ def get_subfiles(path):
         {'path': f.path, 'name': f.name.split('.')[0]} for f in os.scandir(path) if f.is_file()]
     return list_subfiles_path
 
-word_exist={}
 
 def json_to_csv_generator(json_file_path, csv_file_path):
     with open(json_file_path, 'r', encoding='utf8') as file_json:
         word_objects = json.load(file_json)
         hindi_concept_pair = get_english_word_hindi_concept(
             "H_concept-to-mrs-rels.dat")
+        hindi_words = hindi_concept_pair['hindi_words']
+        english_words = hindi_concept_pair['english_words']
+        english_hindi_mapping = generate_english_hindi_map(
+            hindi_words, english_words)
+
         with open(csv_file_path, 'w', encoding='utf8') as file_csv:
-            field_names = ['english', 'hindi_concept', 'type', 'japanese', 'hirangana',
-                           'pronunciation', 'example_eng', 'example_jap']
+            field_names = ['english', 'hindi', 'type', 'pronunciation']
             writer = csv.DictWriter(
                 file_csv, delimiter='|', fieldnames=field_names)
             writer.writeheader()
-            for word, word_definitions in tqdm(word_objects.items()):
-                hindi_concept = ''
-                if word in hindi_concept_pair:
-                    for hindi in hindi_concept_pair[word]:
-                        hindi_concept = hindi_concept+hindi+","
-                hindi_concept = hindi_concept[:-1]
-                pronunciation={}
-                for i, wd in enumerate(word_definitions):
-                    if wd['pronunciation'] in pronunciation:
-                        pronunciation[wd['pronunciation']]=pronunciation[wd['pronunciation']]+1
-                    else:
-                        pronunciation[wd['pronunciation']]=1
-                    writer.writerow({'english': word.replace(' ', '-').lower()+'_'+str(i+1), 'hindi_concept': hindi_concept, 'type': wd['type'], 'japanese': wd['japanese'], 'hirangana': wd['hirangana'],
-                                  'pronunciation': wd['pronunciation']+'_'+str(pronunciation[wd['pronunciation']]),  'example_eng': wd['example_eng'], 'example_jap': wd['example_jap']}) 
+
+            english_hindi_pronounciation = dict(english_hindi_mapping)
+            for hindi_english_word in english_hindi_mapping:
+                # hindi_word = hindi_english_word.split("#")[1]
+                pos = hindi_english_word.split("#")[0].split("_")[1]
+                english_word = hindi_english_word.split("#")[0].split("_")[0]
+                if english_word in word_objects:
+                    word_definition = word_objects[english_word.split("#")[0]]
+                    for element in word_definition:
+                        if element['type'] == pos:
+                            if hindi_english_word in english_hindi_pronounciation:
+                                english_hindi_pronounciation[hindi_english_word].append(
+                                    element['pronunciation'])
+                            else:
+                                english_hindi_pronounciation[hindi_english_word] = [
+                                    element['pronunciation']]
+
+            hindi_word_count = {}
+            english_word_count = {}
+
+            for word, pronunciation in tqdm(english_hindi_pronounciation.items()):
+                hindi_word = word.split("#")[1]
+                pos = word.split("#")[0].split("_")[1]
+                english_word = word.split("#")[0].split("_")[0]
+                concated_pronunciation = ''
+                for element in pronunciation:
+                    concated_pronunciation = concated_pronunciation+element+','
+                concated_pronunciation = concated_pronunciation[:-1]
+
+                if hindi_word in hindi_word_count:
+                    hindi_word_count[hindi_word] += 1
+                else:
+                    hindi_word_count[hindi_word] = 1
+
+                if english_word in english_word_count:
+                    english_word_count[english_word] += 1
+                else:
+                    english_word_count[english_word] = 1
+
+                writer.writerow({'english': english_word+'_'+str(english_word_count[english_word]), 'hindi': hindi_word+'_'+str(
+                    hindi_word_count[hindi_word]), 'type': pos, 'pronunciation': concated_pronunciation})
+
+
+def generate_english_hindi_map(hindi_words, english_words):
+    english_hindi_mapping = {}
+    for word, hindi_meanings in english_words.items():
+        for i, hindi_word in enumerate(hindi_meanings):
+            english_hindi_mapping[word+"#"+hindi_word] = []
+    for word, english_meanings in hindi_words.items():
+        for i, english_word in enumerate(english_meanings):
+            english_hindi_mapping[english_word+"#"+word] = []
+    return english_hindi_mapping
 
 
 def get_english_word_hindi_concept(h_concept_file_path):
     with open(h_concept_file_path, 'r') as file:
-        words = {}
+        english_words = {}
+        hindi_words = {}
         for index, line in enumerate(file):
             h_concept = line.rstrip()
             try:
                 h_concept = h_concept.split()
                 hindi_word = h_concept[1].split('_')[0]
                 english_word = h_concept[2].split('_')[0]
-                if english_word in words:
-                    words[english_word].append(hindi_word)
+                pos = h_concept[3].split('_')[2]
+                english_word_pos = english_word+"_"+extractPos(pos)
+                if english_word_pos in english_words:
+                    english_words[english_word_pos].append(hindi_word)
                 else:
-                    words[english_word] = [hindi_word]
+                    english_words[english_word_pos] = [hindi_word]
+                if hindi_word in hindi_words:
+                    hindi_words[hindi_word].append(english_word_pos)
+                else:
+                    hindi_words[hindi_word] = [english_word_pos]
             except:
-                print("skipping line number:", index+1,
-                      h_concept, " not able to parse")
-        return words
+                pass
+                # print("skipping line number:", index+1,
+                #       h_concept, " not able to parse")
+
+        return {'hindi_words': hindi_words, 'english_words': english_words}
+
+
+def extractPos(pos):
+    if pos == 'v':
+        return "verb"
+    elif pos == 'a':
+        return "adjective"
+    elif pos == 'n':
+        return "noun"
+    elif pos == 'x':
+        return "noun"
+    elif pos == 'c':
+        return "noun"
+    elif pos == 'of':
+        return "noun"
+    elif pos == 'u':
+        return "noun"
+    elif pos == 'q':
+        return "noun"
+    elif pos == 'p':
+        return "preposition"
+    else:
+        return ""
